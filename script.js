99f4501e6a76ada45ba4a9b0b6bdae84dba625ab
dewang <2002thapa@gmail.com>
2024-06-17 10:51:04 +0530

    done

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
+    responsiveClass:true,
+    center:true,
     // nav:true,
     responsive:{
         0:{
-            items:1
+            items:1,
+
         },
         600:{
-            items:3
+            items:3,
+            nav:false
         },
         1000:{
-            items:5
+            items:5,
+            
         }
     }
 })
